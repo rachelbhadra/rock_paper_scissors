642f753827166baf6b175ba5dac1eaa00f5b4e35
Rachel Bhadra <rachelbhadra@berkeley.edu>
2018-05-05 16:09:37 -0700

    Initial Commit

---
+++ Rock Paper Scissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rock Paper Scissors
+//
+//  Created by Rachel Bhadra on 5/5/18.
+//  Copyright © 2018 Rachel Bhadra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
